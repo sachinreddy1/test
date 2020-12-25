b00712429c0dbff34c2186a64eb3d1b3551e2ddb
sachinreddy1 <reddysachin2014@gmail.com>
2020-12-24 21:50:51 -0500

    updates

--- app/src/main/java/com/example/recyclerviewtest/presentation/listener/TimelineListener.kt
+++ app/src/main/java/com/example/recyclerviewtest/presentation/listener/TimelineListener.kt
@@ -11,10 +11,7 @@ class TimelineListener(
 ) : RecyclerView.OnScrollListener(), OnItemTouchListener {
 
     private var mIsMoved = false
-
     private var mXPosition = 0
-    var scrollPosition = 0
-    var scrollPositionOffset = 0
 
     private var mCurrentRVTouched: RecyclerView? = null
     private var mLastTouchedRecyclerView: RecyclerView? = null
@@ -50,7 +47,6 @@ class TimelineListener(
 
             mLastTouchedRecyclerView = rv
         } else if (e.action == MotionEvent.ACTION_CANCEL) {
-            renewScrollPosition(rv)
             rv.removeOnScrollListener(this)
             mIsMoved = false
             mLastTouchedRecyclerView = rv
@@ -79,36 +75,8 @@ class TimelineListener(
     ) {
         super.onScrollStateChanged(recyclerView, newState)
         if (newState == RecyclerView.SCROLL_STATE_IDLE) {
-            renewScrollPosition(recyclerView)
             recyclerView.removeOnScrollListener(this)
             mIsMoved = false
         }
-    }
-
-    /**
-     * This method calculates the current scroll position and its offset to help new attached
-     * recyclerView on window at that position and offset
-     *
-     * @see .getScrollPosition
-     * @see .getScrollPositionOffset
-     */
-    private fun renewScrollPosition(recyclerView: RecyclerView) {
-        val layoutManager = recyclerView.layoutManager as LinearLayoutManager?
-        scrollPosition = layoutManager!!.findFirstCompletelyVisibleItemPosition()
-
-        // That means there is no completely visible Position.
-        if (scrollPosition == -1) {
-            scrollPosition = layoutManager.findFirstVisibleItemPosition()
-
-            // That means there is just a visible item on the screen
-            if (scrollPosition == layoutManager.findLastVisibleItemPosition()) {
-                // in this case we use the position which is the last & first visible item.
-            } else {
-                // That means there are 2 visible item on the screen. However, second one is not
-                // completely visible.
-                scrollPosition += 1
-            }
-        }
-        scrollPositionOffset = layoutManager.findViewByPosition(scrollPosition)!!.left
     }
 }
